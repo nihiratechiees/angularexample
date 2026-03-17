f7fa3092a499dd01566f7869f1b4fa1ef85244b8
nihiratechiees <nihiratechiees@gmail.com>
2026-03-17 19:59:42 +0530

    signal form

--- src/app/RF/login/login.ts
+++ src/app/RF/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,38 +6,45 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { Master } from '../../service/master';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+//import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginModel } from '../../Model/LoginModel';
 import { UserModel } from '../../Model/Usermodel';
+import { form, FormField, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule,
-    MatButtonModule],
+  imports: [ MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule,
+    MatButtonModule,FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
  constructor(private service: Master, private router: Router) { }
 
-  loginobj: LoginModel = {
+  loginobj=signal<LoginModel>({
     username: '',
     password: ''
-  }
+  });
 
-  loginform=new FormGroup({
-    username:new FormControl('',Validators.required),
-    password:new FormControl('',Validators.required)
+  // loginform=new FormGroup({
+  //   username:new FormControl('',Validators.required),
+  //   password:new FormControl('',Validators.required)
+  // })
+
+  loginform=form(this.loginobj,(scemapath)=>{
+    required(scemapath.username),
+    required(scemapath.password)
   })
 
   redirectToRegister() {
     this.router.navigate(['/register']);
   }
 
-  ProceedLogin() {
-    if (this.loginform.valid) {
-      this.loginobj = this.loginform.value as LoginModel;
-      this.service.Userlogin(this.loginobj).subscribe((res:UserModel[]) => {
+  ProceedLogin(event:Event) {
+    event.preventDefault();
+    if (this.loginform().valid()) {
+      //this.loginobj = this.loginform.value as LoginModel;
+      this.service.Userlogin(this.loginobj()).subscribe((res:UserModel[]) => {
         if (res && res.length > 0) {
          // localStorage.setItem('username', res[0].username);
           this.service._isloggedin.set(true);
--- src/app/RF/register/register.ts
+++ src/app/RF/register/register.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+// import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserModel } from '../../Model/Usermodel';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,40 +8,53 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { Master } from '../../service/master';
 import { Router } from '@angular/router';
+import { email, form, FormField, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule,
-    MatButtonModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule,
+    MatButtonModule,FormField],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
-  
+
   constructor(private service: Master, private router: Router) { }
 
-  regiterform=new FormGroup({
-    username:new FormControl('',Validators.compose([Validators.required,Validators.minLength(5)])),
-    password:new FormControl('',Validators.compose([Validators.required,Validators.minLength(8)])),
-    fullname:new FormControl('',Validators.required),
-    email:new FormControl('',Validators.compose([Validators.required,Validators.email])),
-    address:new FormControl(''),
-    isagree:new FormControl(false,Validators.requiredTrue)
-  })
-
-  userobj: UserModel = {
+  // regiterform=new FormGroup({
+  //   username:new FormControl('',Validators.compose([Validators.required,Validators.minLength(5)])),
+  //   password:new FormControl('',Validators.compose([Validators.required,Validators.minLength(8)])),
+  //   fullname:new FormControl('',Validators.required),
+  //   email:new FormControl('',Validators.compose([Validators.required,Validators.email])),
+  //   address:new FormControl(''),
+  //   isagree:new FormControl(false,Validators.requiredTrue)
+  // })
+  userobj = signal<UserModel>({
     username: '',
     password: '',
     fullname: '',
     email: '',
     address: '',
     isagree: false
-  }
+  });
 
-  Proceedregister() {
-    if (this.regiterform.valid) {
-      this.userobj = this.regiterform.value as UserModel;
-      this.service.Userregister(this.userobj).subscribe(res => {
+  regiterform = form(this.userobj, (scemaPath) => {
+    required(scemaPath.username, { message: 'Username is required' }),
+      minLength(scemaPath.username, 5,{message:'Username must be at least 5 characters long'}),
+      required(scemaPath.password, { message: 'Password is required' }),
+      minLength(scemaPath.password, 8,{message:'Password must be at least 8 characters long'}),
+      required(scemaPath.fullname,{ message: 'Fullname is required' }),
+      required(scemaPath.email,{ message: 'Email is required' }),
+      email(scemaPath.email,{ message: 'Please enter valid email' }),
+      required(scemaPath.isagree)
+  })
+
+
+  Proceedregister(event:Event) {
+    event.preventDefault();
+    if (this.regiterform().valid()) {
+     // this.userobj = this.regiterform.value as UserModel;
+      this.service.Userregister(this.userobj()).subscribe(res => {
         alert("Registration Successfull");
         this.redirectToLogin();
       });
